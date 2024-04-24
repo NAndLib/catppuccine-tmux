b69da78fae85e6fd67258da1407629e2fbfe70e6
Andrew Tran <andremail03@gmail.com>
2024-04-23 20:05:00 -0700

    fix(custom/window_current_format.sh): shellcheck

--- custom/window_current_format.sh
+++ custom/window_current_format.sh
@@ -1,14 +1,15 @@
 show_window_current_format() {
-  local number="#I"
-  local background="$thm_gray"
-  local color="$thm_green"
-  local text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
-  local fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")" # number, all, none
+  local number background color text fill key_table window_status_icon_enable current_window_format
+  number="#I"
+  background="$thm_gray"
+  color="$thm_green"
+  text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")" # number, all, none
 
 
-  local key_table="$(tmux display -p '#{client_key_table}')"
+  key_table="$(tmux display -p '#{client_key_table}')"
   if [[ "$key_table" = "off" ]]; then
-    local window_status_icon_enable="$(get_tmux_option "@catppuccin_window_status_icon_enable" "yes")"
+    window_status_icon_enable="$(get_tmux_option "@catppuccin_window_status_icon_enable" "yes")"
     if [[ "$window_status_icon_enable" = "yes" ]]; then
       text="󰌾 $text"
     else
@@ -16,7 +17,7 @@ show_window_current_format() {
     fi
   fi
 
-  local current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
+  current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
 
   echo "$current_window_format"
 }
