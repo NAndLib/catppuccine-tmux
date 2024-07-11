1fb1911113182f3bba1029a241396f0323ab0a96
Andrew Tran <andremail03@gmail.com>
2024-07-11 11:34:28 -0700

    fix: icon for directory

--- custom/directory.sh
+++ custom/directory.sh
@@ -2,7 +2,7 @@ show_directory() {
   local index icon color text module
 
   index=$1
-  icon="$(get_tmux_option "@catppuccin_directory_icon" " ")"
+  icon="$(get_tmux_option "@catppuccin_directory_icon" "")"
   color="$(get_tmux_option "@catppuccin_directory_color" "$thm_pink")"
   text="$(get_tmux_option "@catppuccin_directory_text" "#{b:pane_current_path}")"
 
